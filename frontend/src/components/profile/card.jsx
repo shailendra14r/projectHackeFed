441aaabc784e5cd9a1ab015e4076dc6959b795bc
utkarshsinghakash <146868670+utkarshsinghakash@users.noreply.github.com>
2024-04-13 20:25:49 +0530

    fx

--- frontend/src/components/profile/card.jsx
+++ frontend/src/components/profile/card.jsx
@@ -21,10 +21,11 @@ export default function MediaControlCard() {
     <Card
       sx={{
         display: "flex",
-        minWidth: "500px",
-        maxWidth: "700px",
+        minWidth: "700px",
+        border: "none !important",
         minHeight: "250px",
       }}
+      elevation={0}
     >
       <div
         style={{ display: "flex", flexDirection: "column" }}
@@ -54,20 +55,23 @@ export default function MediaControlCard() {
                 variant="h5"
                 style={{ fontSize: "200%" }}
               >
-                <b>Utkarsh Singh</b>
+                <b style={{ color: "rgb(2, 65, 36)" }}>Utkarsh Singh</b>
               </Typography>
               <Typography
-                style={{ marginTop: "5px", marginBottom: "5px" }}
+                style={{
+                  marginTop: "5px",
+                  marginBottom: "5px",
+                  color: "rgb(2, 65, 36)",
+                }}
                 variant="subtitle1"
                 color="text.secondary"
                 component="div"
               >
-                {" "}
                 <PersonIcon />
                 &nbsp; 57 years
               </Typography>
               <Typography
-                style={{ marginBottom: "5px" }}
+                style={{ marginBottom: "5px", color: "rgb(2, 65, 36)" }}
                 variant="subtitle1"
                 color="text.secondary"
                 component="div"
@@ -75,13 +79,15 @@ export default function MediaControlCard() {
                 <AddLocationIcon /> Noida,UP
               </Typography>
             </div>
-            <div class="card-content-2">
-              <Typography
+            <div class="card-content-2" style={{ marginTop: "50px" }}>
+              {/* <Typography
                 component="div"
                 variant="h5"
-                style={{ fontSize: "200%" }}
+                style={{ fontSize: "1.1rem", color: "rgb(2, 65, 36)" }}
               >
-                Male
+                <ul>
+                  <li>Male</li>
+                </ul>
               </Typography>
               <Typography
                 style={{ marginTop: "5px", marginBottom: "5px" }}
@@ -90,21 +96,38 @@ export default function MediaControlCard() {
                 component="div"
               ></Typography>
               <Typography
-                style={{ marginBottom: "5px" }}
+                style={{ marginBottom: "5px", color: "rgb(2, 65, 36)" }}
                 variant="subtitle1"
                 color="text.secondary"
                 component="div"
               >
-                Suffering from Diabeties
+                <ul>
+                  <li>Suffering from Diabeties</li>
+                </ul>
               </Typography>
               <Typography
-                style={{ marginBottom: "5px" }}
+                style={{ marginBottom: "5px", color: "red" }}
                 variant="subtitle1"
                 color="text.secondary"
                 component="div"
               >
-                Blood grp : A+
-              </Typography>
+                <ul>
+                  <li>Blood grp : A+</li>
+                </ul> */}
+              <div class="list">
+                <ul>
+                  <li style={{ fontSize: "1.1rem", color: "rgb(2, 65, 36)" }}>
+                    Male
+                  </li>
+                  <li style={{ marginBottom: "5px", color: "rgb(2, 65, 36)" }}>
+                    Suffering from Diabeties
+                  </li>
+                  <li style={{ marginBottom: "5px", color: "red" }}>
+                    Blood Group:A+
+                  </li>
+                </ul>
+              </div>
+              {/* </Typography> */}
             </div>
           </div>
           <Box
@@ -113,10 +136,11 @@ export default function MediaControlCard() {
               alignItems: "center",
               fontSize: "200%",
               fontStyle: "bold",
+              color: "red",
             }}
           >
             <VolunteerActivismIcon />
-            &nbsp; Heart
+            &nbsp; Heart, Liver, Eyes
           </Box>
         </CardContent>
       </Box>
--- frontend/src/components/profile/show.jsx
+++ frontend/src/components/profile/show.jsx
@@ -5,45 +5,55 @@ import InfoIcon from "@mui/icons-material/Info";
 
 export default function show() {
   return (
-    <div className="main">
+    <>
       <div className="heading-1">Profile Details</div>
-      <div className="card row">
-        <div className="col-8 offset-4">
-          <Card />
+      <div className="main">
+        <div className="main-1">
+          <div className="card row">
+            <div className="col-8 offset-1">
+              <Card />
+            </div>
+          </div>
+          <div className="contact offset-3">
+            <Button
+              style={{
+                marginTop: "30px",
+                marginLeft: "40px",
+                backgroundColor: "green",
+                color: "white",
+              }}
+              variant="contained"
+            >
+              Contact Now
+            </Button>
+          </div>
+        </div>
+        <div class="main-3"></div>
+        <div class="main-2">
+          <div className="about-box col-8 offset-2">
+            <div className="about">
+              <InfoIcon /> &nbsp;About
+            </div>
+            <br />
+            My name is Utkarsh Singh, and I need a urgent heart for transplant ,
+            I am 57 years old and my blood group is A+. Living with this
+            condition has been incredibly challenging for me and my loved ones.
+            Despite all medical interventions and treatments, my heart function
+            has deteriorated to a point where a transplant is imperative for me
+            to have any chance of leading a healthy life.
+            <br />
+            <br />
+            I understand the complexities and risks associated with organ
+            transplantation, but I am willing to undergo any necessary
+            procedures and adhere to all post-transplant protocols to ensure a
+            successful outcome. Please consider my case with utmost urgency and
+            include me on the transplant waiting list.
+            <br />
+            <br />I am hopeful that with your expertise and assistance, I can
+            receive the life-saving transplant I desperately need.
+          </div>
         </div>
       </div>
-      <div className="contact offset-5">
-        <Button
-          style={{
-            marginTop: "30px",
-            marginLeft: "40px",
-            backgroundColor: "green",
-            color: "white",
-          }}
-          variant="contained"
-        >
-          Contact Now
-        </Button>
-      </div>
-
-      <div className="about-box col-8 offset-4">
-        <div className="about">
-          <hr />
-          <InfoIcon /> &nbsp;About
-        </div>
-        My name is Utkarsh Singh, and I need a urgent heart fro transplant , I
-        am 57 years old and my blood group is A+. Living with this condition has
-        been incredibly challenging for me and my loved ones. Despite all
-        medical interventions and treatments, my heart function has deteriorated
-        to a point where a transplant is imperative for me to have any chance of
-        leading a healthy life. I understand the complexities and risks
-        associated with organ transplantation, but I am willing to undergo any
-        necessary procedures and adhere to all post-transplant protocols to
-        ensure a successful outcome. Please consider my case with utmost urgency
-        and include me on the transplant waiting list. I am hopeful that with
-        your expertise and assistance, I can receive the life-saving transplant
-        I desperately need.
-      </div>
-    </div>
+    </>
   );
 }
