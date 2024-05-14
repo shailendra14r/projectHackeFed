6b845969c69dbaee3ae381b131d9910f8fa4d345
shailendra14r <106880475+shailendra14r@users.noreply.github.com>
2024-05-14 07:39:56 +0530

    Delete frontend directory

--- frontend/src/path.js
+++
@@ -1,2 +0,0 @@
-const path = "http://localhost:5000/";
-export default path;
--- frontend/src/utils/HomeData.js
+++
@@ -1,3 +0,0 @@
-export const bloodTypes = ['A+', 'A-', 'B+', 'B-', 'O+', 'O-', 'AB+', 'AB-'];
-
-export const organs = ['Kidney', 'Liver', 'Pancreas', 'Heart', 'Lungs', 'Blood'];
